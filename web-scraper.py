61a76bf2dc3691392a3b05f34932c86599fa979c
Munseer-am <munnamunseer7@gmail.com>
2022-07-05 20:41:33 +0530

    Image Scrapper

---
+++ web-scraper.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+dst = "/home/munseer/code/python/web-scraped-images/downloads/"
+
+query = str(input("Search: "))
+
+browser = webdriver.Chrome()
+browser.get("https://images.google.com")
+f = browser.find_element("name", "q")
+f.send_keys(query,  u'\ue007')
+html = browser.find_element("xpath", '/html')
+
+for i in range(1,11):
+    try:
+        image = browser.find_element("xpath", f'//*[@id="islrg"]/div[1]/div[{i}]/a[1]/div[1]/img')
+        image.screenshot(f"{dst}/{query.replace(' ', '-')}{i}.png")
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+        html.send_keys(Keys.PAGE_DOWN)
+        continue
+ 
+browser.close()
